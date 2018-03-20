7682137826c000065a097349d0c4d3079c96a97c
Anil S Bodepudi <anilsai.b@gmail.com>
2018-03-20 10:45:55 -0500

    Added Files
    Unit test file and program file are added.

---
+++ Bowling.py
@@ -0,0 +1,41 @@
+maxThrows = 21
+maxFrames = 10
+Score_Strike = 10
+Score_Spare = 10
+
+
+class Bowling:
+    def __init__(self):
+        self.ScArr = []
+
+    def throw(self, pins):
+        if self.numberofThrows() > maxThrows:
+            raise ValueError
+        self.ScArr.append(pins)
+
+    def score(self):
+        score = 0
+        throw = 0
+        frame = 0
+        while frame < maxFrames:
+            if self.ScArr[throw] == Score_Strike:
+                score += self.scoreTwoThrows(throw)
+                throw += 1
+            elif self.ScArr[throw] == Score_Spare:
+                score += self.scoreTwoThrows(throw)
+                throw += 2
+            else:
+                score += self.frameScore(throw)
+                throw += 2
+
+            frame = frame + 1
+        return score
+
+    def numberofThrows(self):
+        return len(self.ScArr) + 1
+
+    def scoreTwoThrows(self, index):
+        return self.ScArr[index] + self.ScArr[index + 1] + self.ScArr[index + 2]
+
+    def frameScore(self, index):
+        return self.ScArr[index] + self.ScArr[index + 1]
---
+++ testbowl.py
@@ -0,0 +1,58 @@
+import unittest as ut
+import Bowling
+
+
+class TestBowling(ut.TestCase):
+
+    def setUp(self):
+        self.bowl = Bowling.Bowling()
+
+    def testforgutter(self):
+        for i in range(20):
+            self.bowl.throw(0)
+
+        assert self.bowl.score() == 0, " zero score"
+
+    def testscore_one_is20(self):
+        for i in range(20):
+            self.bowl.throw(1)
+
+        assert self.bowl.score() == 20
+
+    def testThrowsForMoreThanTwentyOneRolls(self):
+        try:
+            for i in range(22):
+                self.bowl.throw(10)
+        except ValueError:
+            pass
+        else:
+            self.fail("Expected a ValueError")
+
+    # def testBowlforOneSpare(self):
+    #     self.bowl.throw(1)
+    #     self.bowl.throw(9)
+    #     self.bowl.throw(5)
+    #
+    #     for i in range(17):
+    #         self.bowl.throw(0)
+    #
+    #     assert self.bowl.score() == 20
+
+    def testBowlforOneStrike(self):
+        self.bowl.throw(10)
+        self.bowl.throw(1)
+        self.bowl.throw(1)
+
+        for i in range(17):
+            self.bowl.throw(0)
+
+        assert self.bowl.score() == 14
+
+    def testPerfectGameScores300(self):
+        for i in range(12):
+            self.bowl.throw(10)
+
+        assert self.bowl.score() == 300, "Perfect Game should score 300"
+
+    if __name__ == "__main__":
+        ut.main()
